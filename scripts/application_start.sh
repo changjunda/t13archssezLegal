9eaed0bc8b3025f7f8e56b5fd241518dee8fd123
RobinR894 <63579822+RobinR894@users.noreply.github.com>
2021-10-23 02:11:04 +0800

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo su
 cd /home/ec2-user/t13archssezLegal/user/
-sudo bash demo.sh
+sudo bash demo.sh > /dev/null 2> /dev/null < /dev/null &
 cd /home/ec2-user/t13archssezLegal/tagging/
-sudo bash demo.sh
+sudo bash demo.sh > /dev/null 2> /dev/null < /dev/null &
 cd /home/ec2-user/t13archssezLegal/rating/
-sudo bash demo.sh
+sudo bash demo.sh > /dev/null 2> /dev/null < /dev/null &
 cd /home/ec2-user/t13archssezLegal/forum/
-sudo bash demo.sh
+sudo bash demo.sh > /dev/null 2> /dev/null < /dev/null &
