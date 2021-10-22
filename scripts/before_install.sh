660618cb0bd1b399381f8af330d925ef5d7765f9
RobinR894 <63579822+RobinR894@users.noreply.github.com>
2021-10-22 15:34:06 +0800

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
 #create our working directory if it doesn't exist
-DIR="/home/ec2-user/t13archssezLegal"
-if [ -d "$DIR" ]; then
-  echo "${DIR} exists"
-else
-  echo "Creating ${DIR} directory"
-  mkdir ${DIR}
-fi
