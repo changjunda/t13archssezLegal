fe5aa3b2f7c333c6f7f75de81fe34192ec8fa4c0
RobinR894 <63579822+RobinR894@users.noreply.github.com>
2021-10-23 02:54:05 +0800

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-sudo su
 sudo yum -y update
-dockerd > /dev/null 2> /dev/null < /dev/null &
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/t13archssezLegal"
 if [ -d "$DIR" ]; then echo "${DIR} exists"
