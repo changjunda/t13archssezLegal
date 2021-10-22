6888ed7b606c5bd02d7aceab6ddfd18e91c6bbee
RobinR894 <63579822+RobinR894@users.noreply.github.com>
2021-10-23 04:43:24 +0800

    Update demo.sh

--- tagging/demo.sh
+++ tagging/demo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mvn install -f /home/ec2-user/t13archssezLegal/tagging/pom.xml
 docker build -t tagging .
-docker run -p 58081:58081 -t tagging
+docker run -p 58081:58081 -t tagging--memory-swap *
