1b4975a6c5fc0a7e4b85bf37e05b412248a9e79e
RobinR894 <63579822+RobinR894@users.noreply.github.com>
2021-10-22 17:38:53 +0800

    Add files via upload

---
+++ scripts/application_start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd /home/ec2-user/t13archssezLegal/user/
+chmod +x demo.sh
+./demo.sh
+cd /home/ec2-user/t13archssezLegal/tagging/
+chmod +x demo.sh
+./demo.sh
+cd /home/ec2-user/t13archssezLegal/rating/
+chmod +x demo.sh
+./demo.sh
+cd /home/ec2-user/t13archssezLegal/forum/
+chmod +x demo.sh
+./demo.sh
