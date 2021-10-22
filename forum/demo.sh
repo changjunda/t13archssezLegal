4ac61361061ee52f20369558be1b76a9b08e0c29
RobinR894 <63579822+RobinR894@users.noreply.github.com>
2021-10-23 04:48:22 +0800

    Update demo.sh

--- forum/demo.sh
+++ forum/demo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mvn install -f /home/ec2-user/t13archssezLegal/forum/pom.xml
 docker build -t forum .
-docker run -p 58083:58083 -t forum--memory-swap *
+docker run -p 58083:58083 -t forum--memory-swap*
