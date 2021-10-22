3875ef525ff7a9a1069b43d23ae8fab7eb69759d
RobinR894 <63579822+RobinR894@users.noreply.github.com>
2021-10-22 18:34:29 +0800

    Add files via upload

---
+++ scripts/before_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo yum -y update
+#create our working directory if it doesnt exist
+DIR="/home/ec2-user/t13archssezLegal"
+if [ -d "$DIR" ]; then echo "${DIR} exists"
+else echo "Creating ${DIR} directory";
+mkdir ${DIR}
+fi
+sudo dockerd
